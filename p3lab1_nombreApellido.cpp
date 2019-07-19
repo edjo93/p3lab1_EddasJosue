a60f61d8ac1417347d7133f2495f0f34d170e4d1
edjo93 <eddas.carrasco@unitec.edu>
2019-07-19 13:53:37 -0600

    func mostrar matriz done

--- p3lab1_nombreApellido.cpp
+++ p3lab1_nombreApellido.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 void menu();//funcion que muestra un menu en pantalla
 void trabajar_matriz(int**,int,int);
+void mostrar_matriz(int**,int,int);
 int main(){
 	int opcion;
 	//el programa continua mientras el usuario no ingrese la opcion de salir
@@ -41,7 +42,15 @@ void menu(){
 	cout<<"\nopciones\n1.puntos de silla\n2.numeros triangulares\n3.permutaciones\n4.salir\n?:"<<endl;	
 }
 void trabajar_matriz(int**matriz,int filas,int columnas){
-	
+	//1.muestra la matriz mediante una funcion	
 }
 
+void mostrar_matriz(int**matriz,int filas,int columnas){
+	for(int i=0;i<filas;i++){
+		for(int j=0;j<columnas;j++){
+			cout<<"["<<matriz[i][j]<<"]";
+		}
+		cout<<endl;
+	}
+}
 
